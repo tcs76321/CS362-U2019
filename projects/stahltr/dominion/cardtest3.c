9a1748171eb0cbc6680763fdc4cfab9203bc4f28
Trevor Stahl <trevor@trevorstahl.com>
2019-07-19 15:37:17 -0700

    beefed up and tests now too for c3

--- projects/stahltr/dominion/cardtest3.c
+++ projects/stahltr/dominion/cardtest3.c
@@ -21,32 +21,50 @@ int main() {
 	struct gameState testG2, testG3, testG4;
 	int k[10] = {adventurer, embargo, village, minion, mine, cutpurse, sea_hag, tribute, smithy, council_room};
 
-	
+	int iti = 0;
 	
 	//------------- Test 1 ------------------------
 	initializeGame(numPlayers, k, seed, &testG2);
-	printf("TEST 1:verify the player changes in 2 player game\n");
+	
+	printf("TEST 1:verify the player changes 20 times in 2 player game\n");
+	
 	int ccPP = whoseTurn(&testG2);
 	endTurn(&testG2);
 	int ccPPafter = whoseTurn(&testG2);
 	asserttrue(ccPPafter != ccPP);
 	
+	for(iti = 0 ; iti < 20 ; iti++){
+		ccPP = whoseTurn(&testG2);
+		endTurn(&testG2);
+		ccPPafter = whoseTurn(&testG2);
+		asserttrue(ccPPafter != ccPP);
+	}
+	
+	
 	//------------- Test 2 ------------------------
 	initializeGame(numPlayers+1, k, seed, &testG3);
-	printf("TEST 2:verify the player changes in 3 player game\n");
-	ccPP = whoseTurn(&testG3);
-	endTurn(&testG3);
-	ccPPafter = whoseTurn(&testG3);
-	asserttrue(ccPPafter != ccPP);
+	
+	printf("TEST 2:verify the player changes 20 times in 3 player game\n");
+	
+	for(iti = 0 ; iti < 20 ; iti++){
+		ccPP = whoseTurn(&testG3);
+		endTurn(&testG3);
+		ccPPafter = whoseTurn(&testG3);
+		asserttrue(ccPPafter != ccPP);
+	}
+	
 	
 	//------------- Test 3 ------------------------
 	initializeGame(numPlayers+2, k, seed, &testG4);
-	printf("TEST 3:verify the player changes in 4 player game\n");
-	ccPP = whoseTurn(&testG4);
-	endTurn(&testG4);
-	ccPPafter = whoseTurn(&testG4);
-	asserttrue(ccPPafter != ccPP);
 	
+	printf("TEST 3:verify the player changes 20 times in 4 player game\n");
+	
+	for(iti = 0 ; iti < 20 ; iti++){
+		ccPP = whoseTurn(&testG4);
+		endTurn(&testG4);
+		ccPPafter = whoseTurn(&testG4);
+		asserttrue(ccPPafter != ccPP);
+	}
 	
 	
 	
