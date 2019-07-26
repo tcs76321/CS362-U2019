9460215c94aa9c10a01e074c740af966b09f28f9
Trevor Stahl <trevor@trevorstahl.com>
2019-07-26 14:18:11 -0700

    the joys of developing on two OSs

--- projects/stahltr/dominion/randomtestcard1.c
+++ projects/stahltr/dominion/randomtestcard1.c
@@ -23,7 +23,7 @@ int main() {
     int discarded = 1;
     int xtraCoins = 0;
     int shuffledCards = 0;
-    int i, j, m;
+    int j, m;
     int handpos = 0, choice1 = 0, choice2 = 0, choice3 = 0, bonus = 0;
     int remove1, remove2;
     int seed = 1000;
